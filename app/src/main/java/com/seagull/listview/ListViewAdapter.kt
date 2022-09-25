2cd04eaad8e7922e3130f1b612dda82b0d27c1d8
Runner <302716662@qq.com>
2022-09-25 23:05:01 +0800

    添加ViewHolder 优化性能

--- app/src/main/java/com/seagull/listview/ListViewAdapter.kt
+++ app/src/main/java/com/seagull/listview/ListViewAdapter.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
+import android.widget.ListView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class ListViewAdapter(
     private val activity: Activity,
@@ -18,15 +20,36 @@ class ListViewAdapter(
     //position：位置 convertView： View parent
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        //convertView:ListView 优化 convertView：可以被服用的View
+
         //给出position位置的View长什么样
-        val view = LayoutInflater.from(activity).inflate(resourceId, parent, false)
-        val image = view.findViewById<ImageView>(R.id.item_image)
-        val text = view.findViewById<TextView>(R.id.item_text)
+        //优化1.将convertView利用起来，如果convertView不为Null则直接使用，否则新建一个View
+        val view: View
+        val viewHolder: ListViewHolder
+        if (convertView == null) {
+            view = LayoutInflater.from(activity).inflate(resourceId, parent, false)
+            viewHolder = ListViewHolder(view)
+            //通过setTag可以将ViewHolder保存在View中
+            view.tag = viewHolder
+        } else {
+            view = convertView
+            viewHolder = convertView.tag as ListViewHolder
+        }
+
+        //优化2 findViewById：二叉树查找
+        //ViewHolder: 把View保存起来保存到这个中
+//        val image = view.findViewById<ImageView>(R.id.item_image)
+//        val text = view.findViewById<TextView>(R.id.item_text)
 
         //更新image和Text
         val lifeData = data[position]
-        image.setImageResource(lifeData.imageId)
-        text.text = lifeData.name
+        viewHolder.image.setImageResource(lifeData.imageId)
+        viewHolder.text.text = lifeData.name
         return view
     }
+}
+
+class ListViewHolder(view: View) {
+    val image = view.findViewById<ImageView>(R.id.item_image)
+    val text = view.findViewById<TextView>(R.id.item_text)
 }
